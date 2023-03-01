e8f03c2c1068ae43bbfc88461df020b0d6767516
Tokuto0927 <124384547+Tokuto0927@users.noreply.github.com>
2023-03-02 06:49:30 +0900

    1-10 tokuto課題

---
+++ HelloWorld/src/Practice_java/Practice.java
@@ -0,0 +1,13 @@
+package Practice_java;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		int i = 1;
+	    for (int j = 1; j < 11; j++) {
+	        System.out.println(i + 2*j);
+	    }
+
+	}
+
+}
