51999b4650e0af4e9bf69fa9b65bd922bddcfd72
Tokuto0927 <124384547+Tokuto0927@users.noreply.github.com>
2023-03-02 07:16:47 +0900

    1-13 tokuto課題

---
+++ HelloWorld/src/array1/Practice2.java
@@ -0,0 +1,15 @@
+package array1;
+
+public class Practice2 {
+
+	public static void main(String[] args) {
+		int[] a = {1, 2, 3, 4, 5};
+	    Integer[] b = {1, 2, 3};
+	    System.out.println(b[2]);
+	    a[3] = 10;
+	    System.out.println(a[3]);
+	    System.out.println("配列aの要素数は、" + a.length + "です。");
+
+	}
+
+}
