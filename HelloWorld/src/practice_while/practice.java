fbf2b5e7a4baad5a839a9c4c160d2b0819e6e8ee
Tokuto0927 <124384547+Tokuto0927@users.noreply.github.com>
2023-03-02 06:59:03 +0900

    1-12 tokuto課題

---
+++ HelloWorld/src/practice_while/practice.java
@@ -0,0 +1,18 @@
+package practice_while;
+
+public class practice {
+
+	public static void main(String[] args) {
+		int i = 1;
+	    while (i <= 100) {
+	        if (i % 2 == 0) {
+	            System.out.println(i + "は偶数です");
+	        } else {
+	            System.out.println(i + "は奇数です");
+	        }
+	        i++;
+	    }
+
+	}
+
+}
