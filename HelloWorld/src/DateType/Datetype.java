7d0b232d708251605325606700bdf4d3daeeb3f9
Tokuto0927 <124384547+Tokuto0927@users.noreply.github.com>
2023-03-01 23:00:48 +0900

    1-6　tokuto課題

---
+++ HelloWorld/src/DateType/Datetype.java
@@ -0,0 +1,21 @@
+package DateType;
+
+public class Datetype {
+
+	public static void main(String[] args) {
+		int num1 ;
+		int num2 ;
+		num1 = 30;
+		num2 = 30;
+		System.out.println("num1+num2は" + (num1 + num2));
+
+//		問２）
+//		次のプログラムを完成させてください。
+//		String型の name変数 〇〇を自分の名前に変えてください。
+//		System.out.println()を使って「（名前）はjavaを学習しています。」と画面に表示してください。
+		String name = "稲垣徳人";
+        System.out.println(name + "はJavaを学習しています。");
+
+	}
+
+}
---
+++ HelloWorld/src/variable/Variable.java
@@ -0,0 +1,19 @@
+package variable;
+
+public class Variable {
+
+	public static void main(String[] args) {
+				String name = "稲垣徳人";
+				char gender = '男';
+				int age = 24;
+				double height = 179.5;
+
+				System.out.println("私の自己紹介をします。");
+				System.out.println("名前は" + name + "です。");
+				System.out.println("性別は" + gender + "です。");
+				System.out.println("年齢は" + age + "歳です。");
+				System.out.println("身長は" + height + "㎝です。");
+				System.out.println("よろしくお願いします。");
+	}
+
+}
