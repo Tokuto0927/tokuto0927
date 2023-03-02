957122361be2a464ee11e499621964a531595262
Tokuto0927 <124384547+Tokuto0927@users.noreply.github.com>
2023-03-02 20:39:58 +0900

    1-11 tokuto switch型条件分岐

--- HelloWorld/src/practice_switch/sample.java
+++ HelloWorld/src/practice_switch/sample.java
@@ -1,6 +1,19 @@
 package practice_switch;
 
 public class sample {
+	/*
+	問題．switch文を使用し、以下のプログラムを作成してください。
+			変数に数値を代入
+			
+		①2の場合は、「（変数）月は28日です。」
+			出力結果：2月は28日です。
+			
+		②4，6，9，11の場合は、「（変数）月は30日です。」
+			出力例：4月は30日です。
+			
+		③その他の場合は、「（変数）月は31日です。」
+			出力例：1月は31日です。
+*/
 
 	public static void main(String[] args) {
 		int month = 10;
