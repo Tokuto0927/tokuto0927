f5d13cac7cd80594c5106c4c90226e32ee843a31
Tokuto0927 <124384547+Tokuto0927@users.noreply.github.com>
2023-03-02 20:57:05 +0900

    1-14 tokuto 配列②

--- HelloWorld/src/array2/practice.java
+++ HelloWorld/src/array2/practice.java
@@ -1,4 +1,27 @@
 package array2;
+/*
+string型の配列を作成し、アルファベットa～jまでを格納してください。
+brake文を使用し、a～dまで表示してください。
+
+ 出力結果：a
+           b
+           c
+           d
+          
+cotinue文を使用し、i以外を表示してください。
+
+ 出力結果：a
+           b
+           c
+           d
+           e
+           f
+           g
+           h
+           j
+*/
+
+
 
 public class practice {
 
