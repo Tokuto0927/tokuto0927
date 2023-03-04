1c31590ba637c0c13503220258b4379055750189
Tokuto0927 <124384547+Tokuto0927@users.noreply.github.com>
2023-03-04 21:59:15 +0900

    1-28 tokuto オブジェクト指向について

---
+++ HelloWorld/src/animal/Info.java
@@ -0,0 +1,44 @@
+package animal;
+
+import java.util.Scanner;
+
+public class Info {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("コンソールに文字を入力してください");
+
+        String input = scanner.nextLine();
+        String[] animals = input.split(",");
+        
+        for (String animal : animals) {
+            String[] attributes = animal.split(":");
+            String name = attributes[0];
+            double length = Double.parseDouble(attributes[1]);
+            int speed = Integer.parseInt(attributes[2]);
+            String scientificName = getScientificName(name);
+
+            System.out.println("動物名：" + name);
+            System.out.println("体長：" + length + "m");
+            System.out.println("速度：" + speed + "km/h");
+            System.out.println("学名：" + scientificName);
+            System.out.println();
+        }
+    }
+
+    public static String getScientificName(String name) {
+        switch (name) {
+            case "ライオン":
+                return "パンテラ レオ";
+            case "ゾウ":
+                return "ロキソドンタ・サイクロティス";
+            case "パンダ":
+                return "アイルロポダ・メラノレウカ";
+            case "チンパンジー":
+                return "パン・トゥログロディテス";
+            case "シマウマ":
+                return "チャップマンシマウマ";
+            default:
+                return "不明";
+        }
+    }
+}
