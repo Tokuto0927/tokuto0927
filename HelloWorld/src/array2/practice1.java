ff3fa4adcfeb1fa6fe1689ed8f1f45a8408ffa22
Tokuto0927 <124384547+Tokuto0927@users.noreply.github.com>
2023-03-04 09:17:57 +0900

    1-14 tokuto　配列２　修正

---
+++ HelloWorld/src/array2/practice1.java
@@ -0,0 +1,52 @@
+package array2;
+	/*
+	string型の配列を作成し、アルファベットa～jまでを格納してください。
+	brake文を使用し、a～dまで表示してください。
+	
+	 出力結果：a
+	           b
+	           c
+	           d
+	          
+	cotinue文を使用し、i以外を表示してください。
+	
+	 出力結果：a
+	           b
+	           c
+	           d
+	           e
+	           f
+	           g
+	           h
+	           j
+	*/
+
+
+
+public class practice1 {
+
+	public class Main {
+	    public static void main(String[] args) {
+	        // アルファベットa～jまでを格納したString型の配列を作成
+	        String[] alphabets = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
+
+	        // brake文を使用してa～dまで表示
+	        for (int i = 0; i <5; i++) {
+	            if (alphabets[i].equals("e")) {
+	                break;
+	            }
+	            System.out.println(alphabets[i]);
+	        }
+
+	        // cotinue文を使用してi以外を表示
+	        for (int i = 6; i < 10; i++) {
+	            if (alphabets[i].equals("i")) {
+	                continue;
+	            }
+	            System.out.println(alphabets[i]);
+	        }
+	    }
+	}
+
+
+}
