1fc6af07447b1d46176ef56a852a5590232c73c5
Tokuto0927 <124384547+Tokuto0927@users.noreply.github.com>
2023-03-05 00:22:21 +0900

    1-22 tokuto 修正

--- HelloWorld/src/example_other/Main.java
+++ HelloWorld/src/example_other/Main.java
@@ -5,7 +5,12 @@ import example.Sushi;
 
 public class Main {
     public static void main(String[] args) {
-        Sushi sushi = new Sushi("寿司"); // インスタンスの生成
-        sushi.printInfo(); // メソッドの呼び出し
+        Sushi sushi = new Sushi("寿司"); 
+        if (sushi == null) {
+            System.out.println("寿司のインスタンスが存在しません。"); 
+        } else {
+            sushi.printInfo();
+        }
     }
 }
+
