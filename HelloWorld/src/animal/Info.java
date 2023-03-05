d179c132443faabecafed1b96db8f93947693f07
Tokuto0927 <124384547+Tokuto0927@users.noreply.github.com>
2023-03-05 14:31:34 +0900

    1-28 tokuto 修正

--- HelloWorld/src/animal/Info.java
+++ HelloWorld/src/animal/Info.java
@@ -4,32 +4,36 @@ import java.util.Scanner;
 
 public class Info {
     public static void main(String[] args) {
-    	Scanner scanner = new Scanner(System.in);
-    	System.out.println("コンソールに文字を入力してください");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("コンソールに文字を入力してください");
 
-    	String input = scanner.nextLine();
+        String input = scanner.nextLine();
 
-    	if(input != null && !input.isEmpty()) {
-    	    String[] animals = input.split(",");
-    	    for (String animal : animals) {
-    	        String[] attributes = animal.split(":");
-    	        String name = attributes[0];
-    	        double length = Double.parseDouble(attributes[1]);
-    	        int speed = Integer.parseInt(attributes[2]);
-    	        String scientificName = getScientificName(name);
+        if (input == null) {
+            System.out.println("入力がNULLです。");
+            return;
+        }
 
-    	        System.out.println("動物名：" + name);
-    	        System.out.println("体長：" + length + "m");
-    	        System.out.println("速度：" + speed + "km/h");
-    	        System.out.println("学名：" + scientificName);
-    	        System.out.println();
-    	    }
-    	} else {
-    	    System.out.println("入力がNULLまたは空です。");
-    	}
+        if (input.trim().isEmpty()) {
+            System.out.println("入力が空です。");
+            return;
+        }
+
+        String[] animals = input.split(",");
+        for (String animal : animals) {
+            String[] attributes = animal.split(":");
+            String name = attributes[0];
+            double length = Double.parseDouble(attributes[1]);
+            int speed = Integer.parseInt(attributes[2]);
+            String scientificName = getScientificName(name);
+
+            System.out.println("動物名：" + name);
+            System.out.println("体長：" + length + "m");
+            System.out.println("速度：" + speed + "km/h");
+            System.out.println("学名：" + scientificName);
+            System.out.println();
+        }
     }
-
-    
 
     public static String getScientificName(String name) {
         switch (name) {
