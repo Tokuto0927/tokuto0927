305d948f5200e08731607a756fe893670fc6b8c4
Tokuto0927 <124384547+Tokuto0927@users.noreply.github.com>
2023-03-05 14:45:41 +0900

    1-30 tokuto 修正

--- HelloWorld/src/prefecture/Main.java
+++ HelloWorld/src/prefecture/Main.java
@@ -27,18 +27,23 @@ public class Main {
         System.out.println("昇順(1)か降順(2)を選んでください:");
         int order = scanner.nextInt();
 
-        // 都道府県を面積でソート
         if (order == 1) {
             Arrays.sort(prefectures, Comparator.comparing(Prefecture::getArea));
         } else {
             Arrays.sort(prefectures, Comparator.comparing(Prefecture::getArea).reversed());
         }
 
-        // 入力された数字に対応する都道府県を出力
         for (int number : numbers) {
-            System.out.println("都道府県名：" + prefectures[number].getName());
-            System.out.println("県庁所在地：" + prefectures[number].getCapital());
-            System.out.println("面積：" + prefectures[number].getArea() + "km2\n");
+            if (number >= 0 && number < prefectures.length) {
+                Prefecture p = prefectures[number];
+                if (p != null) { 
+                    System.out.println("都道府県名：" + p.getName());
+                    System.out.println("県庁所在地：" + p.getCapital());
+                    System.out.println("面積：" + p.getArea() + "km2\n");
+                }
+            } else {
+                System.out.println("無効な都道府県のインデックスが入力されました。\n");
+            }
         }
     }
 }
