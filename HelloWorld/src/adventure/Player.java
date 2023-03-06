0a565219a6ed5bdad4ea28fbde0dc42c02f12b68
Tokuto0927 <124384547+Tokuto0927@users.noreply.github.com>
2023-03-06 21:26:53 +0900

    1-25 tokuto 再修正

--- HelloWorld/src/adventure/Player.java
+++ HelloWorld/src/adventure/Player.java
@@ -1,12 +1,17 @@
 package adventure;
 
+import java.util.Scanner;
+
 public class Player extends Character {
 	public Player(String name) {
 	    super(name != null ? name : "");
 	}
+	
+	
 
 
     public void greet() {
+    	name = new Scanner(System.in).nextLine();
         System.out.println("こんにちは " + this.name + " さん");
     }
 
