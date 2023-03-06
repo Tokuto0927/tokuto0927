ae622a881dfab7e63c99dc7a59d319debeb0489f
Tokuto0927 <124384547+Tokuto0927@users.noreply.github.com>
2023-03-06 21:51:16 +0900

    1-30 tokuto 再修正

--- HelloWorld/src/prefecture/Prefecture.java
+++ HelloWorld/src/prefecture/Prefecture.java
@@ -1,23 +1,30 @@
 package prefecture;
 
 public class Prefecture {
+    private int num;
     private String name;
     private String capital;
     private double area;
 
-    public Prefecture(String name, String capital, double area) {
+    public Prefecture(int num,String name,String capital,double area) {
+        this.num = num;
         this.name = name;
         this.capital = capital;
         this.area = area;
     }
 
+    public int getNum() {
+        return num;
+    }
+
     public String getName() {
         return name;
     }
-
+    
     public String getCapital() {
-        return capital;
+    	return capital;
     }
+    
 
     public double getArea() {
         return area;
