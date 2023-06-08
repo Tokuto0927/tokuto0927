75bae9ced24bea35d285284af051545d74c535dd
Tokuto0927 <124384547+Tokuto0927@users.noreply.github.com>
2023-06-08 21:08:25 +0900

    curriculumB inagaki

---
+++ NewJava/src/curriculum_B/Qes5.java
@@ -0,0 +1,28 @@
+package curriculum_B;
+
+public class Qes5 {
+
+    public static void main(String[] args) {
+        int rows = 20;
+        int columns = 9;
+
+        // 表の要素を格納する2次元配列を作成
+        String[][] table = new String[rows][columns];
+
+        // 表を作成して要素を埋める
+        for (int j = 0; j < columns; j++) {
+            for (int i = 0; i < rows; i++)  {
+                int result = (i + 1) * (j + 1);
+                table[i][j] = String.format("%03d*%03d=%03d", (i + 1) , (j + 1), result);
+            }
+        }
+
+        // 表の内容を表示
+        for (int j = 0; j < columns; j++) {
+            for (int i = 0; i < rows; i++) {
+                System.out.print(table[i][j] + " || ");
+            }
+            System.out.println();
+        }
+    }
+}
